ecaf22462742853250d42be3fb91ffbb20984aa2
gayashan2020 <gayashanweerasundara@gmail.com>
2025-05-04 20:48:03 +0530

    build: github deploy configs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  // trailing slash for GitHub Pages compatibility
+  trailingSlash: true,
 };
 
 export default nextConfig;
